4583cb266b1ac5111ae854005f0f532a0489c695
Oleg Bordun <oleg.bordun@outlook.com>
2025-12-05 20:47:45 +0100

    read book using sys library

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
   get_words_count,
   get_chars_num,
@@ -21,7 +22,10 @@ def print_report(book_path, num_words, chars_sorted_list):
 
 
 def main():
-  path_to_file = 'books/frankenstein.txt'
+  if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  path_to_file = sys.argv[1]
   text = get_boot_text(path_to_file)
   num_words = get_words_count(text)
   char_dict = get_chars_num(text)
